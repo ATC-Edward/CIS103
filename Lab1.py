b999cfff49e208d39dab8ccc3a17c238fe3f19fc
EdwardMcBride <emcbride312@outlook.com>
2024-09-22 14:11:25 -0500

    'Lab1.py'

---
+++ Lab1.py
@@ -0,0 +1,11 @@
+#
+#Lab1: Getting Started with Python
+#CIS 103: Introduction to Programming
+#Instructor: Edward McBride
+#Student name: Edward McBride
+#Date: 9/7/24
+#Description:
+#This script prints a greeting message to the user.
+#
+#The following line of code prints a greeting message
+print("Hello,welcome to CIS103!")
